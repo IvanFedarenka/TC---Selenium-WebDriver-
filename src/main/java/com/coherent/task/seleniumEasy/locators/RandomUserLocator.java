010bb8d9b46e8323c7df20fff727cd05ef6d1793
Ivan Fedarenka <99142629+Harkness30@users.noreply.github.com>
2022-07-22 15:33:08 +0300

    Delete src/main/java/com/coherent/task/seleniumEasy/locators directory

--- src/main/java/com/coherent/task/seleniumEasy/locators/RandomUserLocator.java
+++
@@ -1,9 +0,0 @@
-package com.coherent.task.seleniumEasy.locators;
-
-import org.openqa.selenium.By;
-
-public class RandomUserLocator {
-
-    public static final By GET_USER_BUTTON = By.cssSelector("button#save");
-    public static final By USERS_PLACE = By.xpath("//div[@id='loading']");
-}
--- src/main/java/com/coherent/task/seleniumEasy/locators/RefreshLocators.java
+++
@@ -1,9 +0,0 @@
-package com.coherent.task.seleniumEasy.locators;
-
-import org.openqa.selenium.By;
-
-public class RefreshLocators {
-
-    public static final By START_BUTTON = By.cssSelector("button#cricle-btn");
-    public static final By PROGRESS = By.cssSelector("div.percenttext");
-}
