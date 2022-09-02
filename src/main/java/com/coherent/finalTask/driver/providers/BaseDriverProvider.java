b1e7f51374b51aa0c051289a62652b373df00925
Ivan Fedarenka <99142629+Harkness30@users.noreply.github.com>
2022-09-02 12:58:17 +0300

    Delete BaseDriverProvider.java

--- src/main/java/com/coherent/finalTask/driver/providers/BaseDriverProvider.java
+++
@@ -1,9 +0,0 @@
-package com.coherent.finalTask.driver.providers;
-
-import org.openqa.selenium.WebDriver;
-
-public class BaseDriverProvider {
-
-    protected static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
-
-}
